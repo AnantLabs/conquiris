73f49175d5ecd6676266e202658f7881f946ab53
andresrc@gmail.com <andresrc@gmail.com@b2a631c2-c7a1-11de-8f3f-2f730f80caef>
2009-11-08 22:33:33 +0000

    Counting collector
    git-svn-id: https://conquiris.googlecode.com/svn/trunk@5 b2a631c2-c7a1-11de-8f3f-2f730f80caef

---
+++ conquiris/conquiris-lucene/src/main/java/net/conquiris/lucene/search/CountingCollector.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.conquiris.lucene.search;
+
+import java.io.IOException;
+
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.Collector;
+import org.apache.lucene.search.Scorer;
+
+/**
+ * A collector that simply counts the number of hits. This collector is not
+ * thread-safe and may not be reused in different searches.
+ * @author Andres Rodriguez
+ */
+public class CountingCollector extends Collector {
+	private int count = 0;
+
+	/** Constructor. */
+	public CountingCollector() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.apache.lucene.search.Collector#acceptsDocsOutOfOrder()
+	 */
+	@Override
+	public boolean acceptsDocsOutOfOrder() {
+		return true;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.apache.lucene.search.Collector#collect(int)
+	 */
+	@Override
+	public void collect(int doc) throws IOException {
+		count++;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.apache.lucene.search.Collector#setScorer(org.apache.lucene.search
+	 * .Scorer)
+	 */
+	@Override
+	public void setScorer(Scorer scorer) throws IOException {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see
+	 * org.apache.lucene.search.Collector#setNextReader(org.apache.lucene.index
+	 * .IndexReader, int)
+	 */
+	@Override
+	public void setNextReader(IndexReader reader, int docBase) throws IOException {
+	}
+
+	/**
+	 * Returns the document count.
+	 * @return The document count
+	 */
+	public final int getCount() {
+		return count;
+	}
+}
