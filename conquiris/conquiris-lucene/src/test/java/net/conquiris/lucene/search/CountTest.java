7f073c5f3a9078137323ec91d3244c28a169a0ff
andresrc@gmail.com <andresrc@gmail.com@b2a631c2-c7a1-11de-8f3f-2f730f80caef>
2009-12-08 12:31:02 +0000

    First tests
    git-svn-id: https://conquiris.googlecode.com/svn/trunk@20 b2a631c2-c7a1-11de-8f3f-2f730f80caef

---
+++ conquiris/conquiris-lucene/src/test/java/net/conquiris/lucene/search/CountTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2009 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.conquiris.lucene.search;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.testng.annotations.Test;
+
+/**
+ * Counting queries tests.
+ * @author Andres Rodriguez
+ */
+public class CountTest {
+	private final TestIndex index = new TestIndex();
+
+	public CountTest() {
+	}
+
+	private int count() throws IOException {
+		return index.perform(CountingInquiry.of(new MatchAllDocsQuery())).getTotalHits();
+	}
+
+	@Test
+	public void load() throws IOException {
+		index.add(1, 1);
+	}
+
+	@Test(dependsOnMethods = "load")
+	public void count1() throws IOException {
+		assertEquals(count(), 1);
+	}
+}
