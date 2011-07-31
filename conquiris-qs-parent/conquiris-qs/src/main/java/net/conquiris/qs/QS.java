fa9fc9779bca80cd1aa8c05a4b2edb9167a5baf8
andresrc@gmail.com <andresrc@gmail.com@b2a631c2-c7a1-11de-8f3f-2f730f80caef>
2011-07-31 16:02:36 +0000

    [qs] Dictionary building
    git-svn-id: https://conquiris.googlecode.com/svn/trunk@37 b2a631c2-c7a1-11de-8f3f-2f730f80caef

--- conquiris-qs-parent/conquiris-qs/src/main/java/net/conquiris/qs/QS.java
+++ conquiris-qs-parent/conquiris-qs/src/main/java/net/conquiris/qs/QS.java
@@ -19,7 +19,14 @@ import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
 
+import javax.annotation.Nullable;
+import javax.annotation.concurrent.NotThreadSafe;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
 import com.google.common.collect.ImmutableBiMap;
 
 /**
@@ -27,15 +34,43 @@ import com.google.common.collect.ImmutableBiMap;
  * @author Andres Rodriguez
  */
 public final class QS {
+	private static final QS EMPTY = new QS(ImmutableBiMap.<Class<? extends QueryToken>, String> of());
+	private static final QS DEFAULT = new Builder(EMPTY).build();
+
 	/** Query tokens map. */
-	private final ImmutableBiMap<Class<? extends QueryToken>, String> tokens = ImmutableBiMap.of();
+	private final ImmutableBiMap<Class<? extends QueryToken>, String> queries;
+
+	public static QS get() {
+		return DEFAULT;
+	}
+
+	/**
+	 * Creates a new builder starting with an existing dictionary.
+	 * @param qs Dictionary to use.
+	 * @return A new builder.
+	 */
+	public static Builder builder(QS qs) {
+		return new Builder(qs);
+	}
+
+	/**
+	 * Creates a new builder starting with the default dictionary.
+	 * @return A new builder.
+	 */
+	public static Builder builder() {
+		return new Builder(DEFAULT);
+	}
 
 	private QueryToken checkQuery(QueryToken query) {
 		return checkNotNull(query, "Null query");
 	}
 
-	/** Default constructor. */
-	QS() {
+	/**
+	 * Constructor.
+	 * @param queries Query dictionary.
+	 */
+	private QS(Map<Class<? extends QueryToken>, String> queries) {
+		this.queries = ImmutableBiMap.copyOf(queries);
 	}
 
 	/**
@@ -45,13 +80,13 @@ public final class QS {
 	 * @throws IllegalArgumentException if the query type is unknown.
 	 */
 	String getQueryKey(QueryToken query) {
-		String key = tokens.get(query.getClass());
-		checkArgument(key != null, "Unknown query type [%s]", query.getClass());
+		String key = queries.get(query.getClass());
+		checkArgument(key != null, "Unknown query type [%s]", query.getClass().getName());
 		return key;
 	}
 
 	public void write(QueryToken query, Appendable a) throws IOException {
-		doWrite(checkNotNull(query), a);
+		doWrite(checkQuery(query), a);
 	}
 
 	private void doWrite(QueryToken query, Appendable a) throws IOException {
@@ -72,6 +107,85 @@ public final class QS {
 	public QueryToken parse(String string) {
 		// TODO;
 		return MatchAll.get();
+	}
+
+	/**
+	 * Builder for QS objects.
+	 * @author Andres Rodriguez
+	 */
+	@NotThreadSafe
+	public static class Builder {
+		private final QS previous;
+		private BiMap<Class<? extends QueryToken>, String> current = null;
+
+		private Builder(QS previous) {
+			this.previous = checkNotNull(previous);
+		}
+
+		private BiMap<Class<? extends QueryToken>, String> checkMap() {
+			return (current != null) ? current : previous.queries;
+		}
+
+		private void checkEntry(BiMap<Class<? extends QueryToken>, String> map, Class<? extends QueryToken> queryType,
+				String key) {
+			checkNotNull(queryType);
+			checkNotNull(key);
+			checkArgument(!map.containsKey(queryType), "Duplicate query type [%s]", queryType.getName());
+			checkArgument(!map.containsValue(key), "Duplicate query key [%s]", key);
+		}
+
+		private BiMap<Class<? extends QueryToken>, String> current() {
+			if (current == null) {
+				current = HashBiMap.create(previous.queries);
+			}
+			return current;
+		}
+
+		/**
+		 * Adds a query type to the QS dictionary.
+		 * @param queryType Query type.
+		 * @param key Key to use.
+		 * @return This builder.
+		 * @throws IllegalArgumentException if the key is invalid or the key and/or query type are
+		 *           already being used.
+		 */
+		public Builder put(Class<? extends QueryToken> queryType, String key) {
+			checkEntry(checkMap(), queryType, key);
+			current().put(queryType, key);
+			return this;
+		}
+
+		/**
+		 * Adds a collection of query types to the QS dictionary. If an exception is thrown no
+		 * modification is made to the dictionary.
+		 * @param queries Query types to add. If {@code null} or empty no action is performed.
+		 * @return This builder.
+		 * @throws IllegalArgumentException if any of keys is invalid or any of the keys and/or querys
+		 *           type are already being used.
+		 */
+		public Builder put(@Nullable Map<Class<? extends QueryToken>, String> queries) {
+			if (queries == null || queries.isEmpty()) {
+				return this;
+			}
+			BiMap<Class<? extends QueryToken>, String> map = checkMap();
+			for (Entry<Class<? extends QueryToken>, String> e : queries.entrySet()) {
+				checkEntry(map, e.getKey(), e.getValue());
+			}
+			current().putAll(queries);
+			return this;
+		}
+
+		/**
+		 * Builds the query dictionary.
+		 * @return The requested dictionay.
+		 */
+		public QS build() {
+			if (current == null) {
+				return previous;
+			}
+			return new QS(current);
+		}
+
 	}
 
 }
